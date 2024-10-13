4c6a68c6cac7a8954fdedc86a465691f1a87d090
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-10-13 13:02:53 -0500

    Styling categories finished

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue'
 import BaseTitle from '@/components/BaseTitle.vue';
 import useAPI from '@/composables/useAPI'
+import { RouterLink } from 'vue-router';
 
 const { categories, getCategories } = useAPI()
 
@@ -14,7 +15,13 @@ onMounted(async () => {
 <template>
 <BaseTitle>Trivia Application</BaseTitle>
 
-<div v-for="category in categories" :key ="category.id">
+<div class="text-slate-700 grid flex-grow grid-cols-4 gap-12 m-20">
+<RouterLink v-for="category in categories" :key ="category.id"
+:to="''" 
+class="bg-pink-100 text-center flex h-32 items-center justify-center rounded-lg border-4 border-pink-300 py-4 font-bold uppercase hover:cursor-pointer hover:border-purple-400 hover:bg-purple-200 hover:text-purple-900 trasition-colors duration-300"
+>
   {{ category.name }}
+</RouterLink>
+
   </div>
 </template>
